24f092985114904de6e1b7cf0a7c3dce92a6f4a3
pezayo-physical <pzyinnju@163.com>
2025-12-07 07:28:51 +0800

    [fix]: remove top_p param

--- anthropic_proxy.py
+++ anthropic_proxy.py
@@ -619,6 +619,7 @@ async def messages_endpoint(request: Request):
 
         # 检查是否为流式请求
         is_stream = request_data.get('stream', False)
+        request_data.pop("top_p")
 
         # 获取请求头
         headers = dict(request.headers)
